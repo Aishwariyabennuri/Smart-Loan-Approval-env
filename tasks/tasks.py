06658bf1fdb7d2647c1dca45f63e57a25c8c45d8
pagidimarri-meghana08 <meghanapagidimarri@gmail.com>
2026-04-11 13:48:40 +0530

    Update tasks.py

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,1 +1,13 @@
-TASKS = ["easy", "medium", "hard"]
+# tasks.py
+from enum import Enum
+
+class Task(str, Enum):
+    EASY = "easy"
+    MEDIUM = "medium"
+    HARD = "hard"
+
+TASK_DESCRIPTIONS = {
+    Task.EASY:   "Stable income, good credit, low loan amount",
+    Task.MEDIUM: "Stable income, bad credit, high loan amount",
+    Task.HARD:   "Unstable income, self-employed, very high loan amount"
+}
